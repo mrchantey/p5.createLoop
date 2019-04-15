c0a1590d7a0c7fe707fb3faf6a00f24a20ef2e73
Peter Hayman <peteyhayman@gmail.com>
2019-04-15 13:22:47 +1000

    patch

--- src/p5.createLoop.js
+++ src/p5.createLoop.js
@@ -27,7 +27,6 @@ function attachCreateLoop() {
         if (options2 !== undefined)
             Object.assign(loopOptions, options2)
 
-        console.dir(loopOptions);
         sketch.animLoop = createLoop(loopOptions)
         if (sketch._isGlobal)
             window.animLoop = sketch.animLoop
