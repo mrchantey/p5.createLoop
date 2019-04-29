2798fc8725f3d8e19a29579f60dfd903d56fd983
Peter Hayman <peteyhayman@gmail.com>
2019-04-30 01:16:17 +1000

    patch

--- scripts/prepublish.ps1
+++ scripts/prepublish.ps1
@@ -1,8 +1,8 @@
 Write-Output "patching version.."
 git.exe add .
 git.exe commit -m 'patch'
-npm.cmd version patch
-npm.cmd version
+# npm.cmd version patch
+# npm.cmd version
 Write-Output "building project.."
 npm.cmd run build
 Write-Output "updating README version.."
